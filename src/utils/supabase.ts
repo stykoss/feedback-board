41af39e28e9d93314a5942c509ab5729bfd298eb
Guillaume Danguy <42386930+stykoss@users.noreply.github.com>
2023-01-16 13:15:31 +0100

    add api

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  'https://cohylndywsyzfqqvqmic.supabase.co',
+  'https://pubtrgqgfzizxxyawohm.supabase.co',
   `${import.meta.env.VITE_SUPABASE_KEY}`
 );
 
